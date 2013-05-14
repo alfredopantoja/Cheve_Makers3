0dd2ccd89d4c2210d34e40349fff28bf6fe04ff3
Alfredo Pantoja <chololoy@gmail.com>
2013-05-14 12:01:57 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CheveMakers3::Application.config.secret_token = '2731387fb1eaf9b197e4f6bc97c351ad471476df374d457cb59abd808cd3df7923a0b30f5c91a442399469856cfd2db76efbb13e8e48fd346e8c7d91d2350c4e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CheveMakers3::Application.config.session_store :cookie_store, key: '_cheve_makers3_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CheveMakers3::Application.config.session_store :active_record_store
